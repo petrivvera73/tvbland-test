921be1192c2e9f04133239d8ece2c8a0cb5063e3
developer <petrivvera73@gmail.com>
2022-12-16 19:58:29 +0200

    UPD: request endpoint changed

--- src/store/shows/showsThunk.ts
+++ src/store/shows/showsThunk.ts
@@ -1,13 +1,25 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios, { AxiosError } from 'axios';
+import { IShow } from '../../types/shows';
 import { IFetchError } from '../../types/utils';
 
 export const getShows = createAsyncThunk(
   'shows/getShows',
   async (_, { rejectWithValue }) => {
     try {
-      const { data } = await axios.get('/shows');
-      return data || [];
+      const today = new Date();
+      const todayString = `${today.getFullYear().toString()}-${(
+        today.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
+      const { data } = await axios.get(`/schedule?date=${todayString}`);
+      const remappedData = data
+        .map((item: { show: IShow }) => item.show)
+        .filter((show: IShow, index: number, array: IShow[]) => {
+          return array.findIndex((e) => e.id === show.id) === index;
+        });
+      return remappedData || [];
     } catch (err) {
       const error = err as AxiosError<IFetchError>;
       if (!error.response) {
